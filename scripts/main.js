ef9523d6bc7d77c9dd3be66974c47db4cf7793c0
William Nigbur <7578002@isd535.org>
2021-12-01 09:14:37 -0600

    added everything from coffe runner but glitch in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
 (function (window) {
- 'use strict'
+    'use strict';
+
+    const FORM_SELECTOR = '[data-coffee-order="form"]';
+    const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    const SERVER_URL = 'https://coffeerun-V2-rest-api.herokuapp.com/api/coffeeorders';
+
+    let App = window.App;
+    let Storage = Storage;
+
+    let remoteDS = new RemoteDataStore(SERVER_URL);
+
+    let myStorage = new Storage('001', remoteDS);
+    let checklist = new CheckList(CHECKLIST_SELECTOR);
+    checklist.addClickHandler(myStorage.deliverOrder.bind(myStorage))
+    
+    window.myStorage = myStorage;
+
+    let formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSubmitHandler(function (data) {
+        myStorage.createOrder.call(myStorage, data);
+        checklist.addRow.call(checklist, data);
+    });
+
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+
 })(window);
--- scripts/storage.js
+++ scripts/storage.js
@@ -3,32 +3,32 @@
 
     let App  = window.App || {};
 
-    function Truck(truckId, db){
-        this.truckId = truckId;
+    function Storage(storageId, db){
+        this.storageId = storageId;
         this.db = db;
     }
 
-    Truck.prototype.createOrder = function (order) {
+    Storage.prototype.createOrder = function (order) {
         console.log('Adding order for ' + order.emailAddress);
         this.db.add(order.emailAddress, order);
     }
 
-    Truck.prototype.deliverOrder = function (customerId) {
-        console.log('Delivering order for ' + customerId);
+    Storage.prototype.deliverOrder = function (customerId) {
+        console.log(customerId + " has picked up their order.");
         this.db.remove(customerId);
     }
 
-    Truck.prototype.printOrders = function() {
+    Storage.prototype.printOrders = function() {
 
         let customerIdArray = Object.keys(this.db.getAll());
 
-        console.log('Truck#' + this.truckId + ' has pending orders:');
+        console.log('Storage#' + this.storageId + ' has pending orders:');
         customerIdArray.forEach(function (id) {
             console.log(this.db.get(id));
         }.bind(this));
     }
 
-    App.Truck = Truck;
+    App.Storage = Storage;
     window.App = App;
 
 }) (window);
